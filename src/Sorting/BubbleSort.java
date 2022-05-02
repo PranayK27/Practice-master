108661d8693e7ec12c516a6aad1566940b257ebb
Pranay <kekre.pranay@gmail.com>
2022-05-02 17:05:45 +0100

    all-types-of-sorting

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,39 @@
+package Sorting;
+
+public class BubbleSort {
+    static void bubbleSort(int arr[], int n)
+    {
+        if (n == 1)                     //passes are done
+        {
+            return;
+        }
+
+        for (int i=0; i<n-1; i++)       //iteration through unsorted elements
+        {
+            if (arr[i] > arr[i+1])      //check if the elements are in order
+            {                           //if not, swap them
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+
+        bubbleSort(arr, n-1);           //one pass done, proceed to the next
+    }
+
+    void display(int arr[])                 //display the array
+    {
+        for (int i=0; i<arr.length; ++i)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {6, 4, 5, 12, 2, 11, 9};
+        bubbleSort(arr, arr.length);
+        ob.display(arr);
+    }
+}
