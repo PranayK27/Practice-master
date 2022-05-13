1711328c70e1e993d2e86e183f36795087f3edfb
Pranay Kekre <kekrepranay@gmail.com>
2022-05-13 19:10:29 +0100

    minor

--- src/Collections/Experiments/ListToIntArray.java
+++ src/Collections/Experiments/ListToIntArray.java
@@ -1,11 +1,12 @@
 package Collections.Experiments;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListToIntArray {
     public static void main(String[] args) {
 
-        List<Integer> list = new java.util.ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for(int i = 10; i<=20; i++){
             list.add(i);
         }
