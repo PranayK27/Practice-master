f1845c1d4a9daa0687257d98fe900069c28ea7fb
PranayK27 <kekre.pranay@gmail.com>
2022-12-05 19:51:32 +0000

    dsa

--- src/interview/DSA/RotateArray.java
+++ src/interview/DSA/RotateArray.java
@@ -6,9 +6,12 @@ import java.util.logging.Logger;
 
 public class RotateArray {
     public static void main(String[] args) {
+
+        Logger logger = Logger.getLogger(RotateArray.class.getName());
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Number of elements: ");
+        logger.info("Number of elements: ");
         int n = sc.nextInt();
         //array
         int[] array = new int[n];
@@ -17,21 +20,26 @@ public class RotateArray {
             array[i] = sc.nextInt();
         }
 
-        System.out.print("Enter the steps: ");
+        logger.info("Enter the steps: ");
         int s = sc.nextInt();
 
         Helper rotateHelper = new Helper();
-        System.out.println(Arrays.toString(rotateHelper.rotate(array, s)));
+        logger.info(Arrays.toString(rotateHelper.rotate(array, s)));
+        logger.info(Arrays.toString(rotateHelper.rotate2(array, s)));
+        logger.info(Arrays.toString(rotateHelper.rotate3(array, s)));
+
     }
 }
 
 class Helper{
     Logger logger = Logger.getLogger(Helper.class.getName());
+
+    //Solution 1
     public int[] rotate(int[] nums, int k) {
         if(k > nums.length)
             k=k%nums.length;
-            logger.info(String.valueOf(k));
-            logger.info(String.valueOf(nums.length));
+//            logger.info(String.valueOf(k));
+//            logger.info(String.valueOf(nums.length));
         int[] result = new int[nums.length];
         for(int i=0; i < k; i++){
             result[i] = nums[nums.length-k+i];
@@ -43,6 +51,48 @@ class Helper{
             j++; }
         System.arraycopy(result, 0, nums, 0, nums.length);
         return result;
+    }
+
+    //Solution 2
+    public static int[] rotate2(int[] arr, int order) {
+        if (arr == null || arr.length == 0 || order < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        for (int i = 0; i < order; i++) {
+            for (int j = arr.length - 1; j > 0; j--) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+        return arr;
+    }
+
+    //Solution 3
+    public static int[] rotate3(int[] arr, int order) {
+        if (arr == null || arr.length==0 || order < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+        if(order > arr.length){
+            order = order %arr.length;
+        }
+        //length of first part
+        int a = arr.length - order;
+        reverse(arr, 0, a-1);
+        reverse(arr, a, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+        return arr;
+    }
+    public static void reverse(int[] arr, int left, int right){
+        if(arr == null || arr.length == 1)
+            return;
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
     }
 
 }
