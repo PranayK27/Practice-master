7e69dfd723d2c11ec35725e4ae7d01e5dff7c98d
PranayK27 <kekre.pranay@gmail.com>
2022-12-21 15:36:40 +0000

    codechef

---
+++ src/CodeChef/Self/ApplesAndOranges.java
@@ -0,0 +1,77 @@
+package CodeChef.Self;
+
+import java.util.Scanner;
+
+/*
+* Problem
+Bob has X rupees and goes to a market. The cost of apples is Rs. A per kg and the cost of oranges is Rs. B per kg.
+
+Determine whether he can buy at least 11 kg each of apples and oranges.
+
+Input Format
+The first line of input will contain an integer X, the amount of money Bob has.
+The second line of input contains two space-separated integers A and B, the cost per kg of apples and oranges respectively.
+Output Format
+Print a single line containing Yes if Bob can buy the fruits and No otherwise.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings yes, Yes, yEs, and YES will all be treated as identical).
+
+Constraints
+1 \leq X,A,B \leq 10^51≤X,A,B≤10
+5
+
+Subtasks
+Subtask 1 (100 points):
+Original constraints.
+Sample 1:
+Input
+14
+2 2
+Output
+Yes
+Explanation:
+The cost of buying 1 kg each of apple and orange is 2+2=4. Since Bob has more than 4 rupees, the answer will be Yes.
+
+Sample 2:
+Input
+1
+1 1
+Output
+No
+Explanation:
+Bob can only buy either 1 kg of apples or 1 kg of oranges with 1 rupee, hence the answer is No.
+
+Sample 3:
+Input
+5
+3 2
+Output
+Yes
+Explanation:
+The cost of buying 1 kg each of apple and orange is 3+2=5. Since Bob has exactly 5 rupees, the answer will be Yes.
+
+Sample 4:
+Input
+Output
+10000
+5000 6000
+Output
+No
+Explanation:
+The cost of buying 1 kg each of apple and orange is 5000+6000=11000. Since Bob has only 10000 rupees, the answer will be No.
+* */
+
+public class ApplesAndOranges {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rs=sc.nextInt();
+        int AperKg=sc.nextInt();
+        int OperKg=sc.nextInt();
+
+        if(rs>=(AperKg+OperKg)){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
+        }
+    }
+}
---
+++ src/CodeChef/Self/Chefandchapters.java
@@ -0,0 +1,53 @@
+package CodeChef.Self;
+
+import java.util.Scanner;
+
+/**
+ * Problem
+ * This semester, Chef took XX courses. Each course has YY units and each unit has ZZ chapters in it.
+ *
+ * Find the total number of chapters Chef has to study this semester.
+ *
+ * Input Format
+ * The first line will contain TT, the number of test cases. Then the test cases follow.
+ * Each test case consists of a single line of input, containing three space-separated integers X, Y,X,Y, and ZZ.
+ * Output Format
+ * For each test case, output in a single line the total number of chapters Chef has to study this semester.
+ *
+ * Constraints
+ * 1 \leq T \leq 10001≤T≤1000
+ * 1 \leq X, Y, Z \leq 10001≤X,Y,Z≤1000
+ * Sample 1:
+ * Input
+ * 3
+ * 1 1 1
+ * 2 1 2
+ * 1 2 3
+ * Output
+ * 1
+ * 4
+ * 6
+ * Explanation:
+ * Test case 1: There is only 1 course with 1 unit. The unit has 1 chapter. Thus, the total number of chapters is 1.
+ *
+ * Test case 2: There are 2 courses with 1 unit each. Thus, there are 2 units. Each unit has 2 chapters. Thus, the total number of chapters is 4.
+ *
+ * Test case 3: There is only 1 course with 2 units. Each unit has 3 chapters. Thus, the total number of chapters is 6.
+ * */
+
+public class Chefandchapters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int x=0; x<T; x++) {
+            int courses=sc.nextInt();
+            int units=sc.nextInt();
+            int chapters=sc.nextInt();
+
+            int numOfChap = courses*units*chapters;
+
+            System.out.println(numOfChap);
+        }
+    }
+}
