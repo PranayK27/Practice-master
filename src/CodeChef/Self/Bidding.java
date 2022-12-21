f72b2384e64e1aa8571cae571dbc4c46bc231f19
PranayK27 <kekre.pranay@gmail.com>
2022-12-21 14:51:15 +0000

    codechef

---
+++ src/CodeChef/Self/Bidding.java
@@ -0,0 +1,67 @@
+package CodeChef.Self;
+
+import java.util.Scanner;
+
+/** Problem */
+/**
+* Alice, Bob and Charlie are bidding for an artifact at an auction.
+Alice bids AA rupees, Bob bids BB rupees, and Charlie bids CC rupees (where AA, BB, and CC are distinct).
+
+According to the rules of the auction, the person who bids the highest amount will win the auction.
+Determine who will win the auction.
+
+Input Format
+The first line contains a single integer TT — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains three integers AA, BB, and CC, — the amount bid by Alice, Bob, and Charlie respectively.
+Output Format
+For each test case, output who (out of Alice, Bob, and Charlie) will win the auction.
+
+You may print each character of Alice, Bob, and Charlie in uppercase or lowercase (for example, ALICE, aliCe, aLIcE will be considered identical).
+
+Constraints
+1 \leq T \leq 10001≤T≤1000
+1 \leq A, B, C \leq 10001≤A,B,C≤1000
+AA, BB, and CC are distinct.
+Sample 1:
+* Input
+4
+200 100 400
+155 1000 566
+736 234 470
+124 67 2
+* Output
+Charlie
+Bob
+Alice
+Alice
+*
+Explanation:
+Test Case 1: Charlie wins the auction since he bid the highest amount.
+
+Test Case 2: Bob wins the auction since he bid the highest amount.
+
+Test Case 3: Alice wins the auction since she bid the highest amount.
+
+Test Case 4: Alice wins the auction since she bid the highest amount.
+* */
+
+public class Bidding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int x=0; x<T; x++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+
+            if(a>b && a>c){
+                System.out.println("Alice");
+            } else if(b>a && b>c){
+                System.out.println("Bob");
+            } else if(c>a && c>b){
+                System.out.println("Charlie");
+            }
+        }
+    }
+}
---
+++ src/CodeChef/Self/Podiumfinish.java
@@ -0,0 +1,60 @@
+package CodeChef.Self;
+
+import java.util.Scanner;
+
+/** Problem */
+/**
+ * Chef got his dream seat in F1 and secured a 3^{rd}3
+ * rd
+ *   place in his debut race. He now wants to know the time gap between him and the winner of the race.
+ *
+ * You are his chief engineer and you only know the time gap between Chef and the runner up of the race, given as AA seconds, and the time gap between the runner up and the winner of the race, given as BB seconds.
+ *
+ * Please calculate and inform Chef about the time gap between him and the winner of the race.
+ *
+ * Input Format
+ * The first line of input will contain a single integer TT, denoting the number of test cases.
+ * Each test case consists of a single line of input containing two space-separated integers AA and BB denoting the time gap between Chef and the runner up and the time gap between the runner up and the winner respectively.
+ * Output Format
+ * For each test case, output the time gap between Chef and the winner of the race.
+ *
+ * Constraints
+ * 1 \leq T \leq 1001≤T≤100
+ * 1 \leq A, B \leq 101≤A,B≤10
+ * Sample 1:
+ * Input
+ * 4
+ * 1 1
+ * 2 5
+ * 3 2
+ * 5 6
+ * Output
+ * 2
+ * 7
+ * 5
+ * 11
+ * Explanation:
+ * Test case 1: The time gap between Chef and runner up is 1 second. The time gap between runner up and the winner is 1 second. Thus, the time gap between Chef and the winner is 1+1=2 seconds.
+ *
+ * Test case 2: The time gap between Chef and runner up is 2 seconds. The time gap between runner up and the winner is 5 second. Thus, the time gap between Chef and the winner is 2+5=7 seconds.
+ *
+ * Test case 3: The time gap between Chef and runner up is 3 seconds. The time gap between runner up and the winner is 2 second. Thus, the time gap between Chef and the winner is 3+2=5 seconds.
+ *
+ * Test case 4: The time gap between Chef and runner up is 5 seconds. The time gap between runner up and the winner is 6 second. Thus, the time gap between Chef and the winner is 5+6=11 seconds.
+* */
+
+public class Podiumfinish {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int x=0; x<T; x++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int total = a + b;
+            System.out.println(total);
+        }
+    }
+}
