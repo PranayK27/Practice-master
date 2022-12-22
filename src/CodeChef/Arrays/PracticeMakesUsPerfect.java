50e35e6f774e5238c3c8c7829ecece5734e748fc
PranayK27 <kekre.pranay@gmail.com>
2022-12-22 18:13:54 +0000

    codechef *1

---
+++ src/CodeChef/Arrays/PracticeMakesUsPerfect.java
@@ -0,0 +1,93 @@
+package CodeChef.Arrays;
+
+import java.util.Scanner;
+
+/*
+Problem
+Most programmers will tell you that one of the ways to improve your performance in competitive programming is to practice a lot of problems.
+
+Our Chef took the above advice very seriously and decided to set a target for himself.
+
+Chef decides to solve at least 1010 problems every week for 44 weeks.
+Given the number of problems he actually solved in each week over 44 weeks as P_1, P_2, P_3,P
+1
+​
+ ,P
+2
+​
+ ,P
+3
+​
+ , and P_4P
+4
+​
+ , output the number of weeks in which Chef met his target.
+
+Input Format
+There is a single line of input, with 44 integers P_1, P_2, P_3,P
+1
+​
+ ,P
+2
+​
+ ,P
+3
+​
+ , and P_4P
+4
+​
+ . These are the number of problems solved by Chef in each of the 44 weeks.
+
+Output Format
+Output a single integer in a single line - the number of weeks in which Chef solved at least 1010 problems.
+
+Constraints
+
+1 ≤ P1, P2, P3, P4 ≤ 100
+Sample 1:
+Input
+Output
+12 15 8 10
+3
+Explanation:
+Chef solved at least 10 problems in the first, second and fourth weeks. He failed to solve at least 10 problems in the third week. Hence, the number of weeks in which Chef met his target is 3.
+
+Sample 2:
+Input
+2 3 1 10
+Output
+1
+Explanation:
+Chef solved at least 10 problems in the fourth week. He failed to solve at least 10 problems in all the other three weeks. Hence, the number of weeks in which Chef met his target is 1.
+
+Sample 3:
+Input
+12 100 99 11
+Output
+4
+Explanation:
+Chef solved at least 10 problems in all the four weeks. Hence, the number of weeks in which Chef met his target is 4.
+
+Sample 4:
+Input
+1 1 1 1
+Output
+0
+Explanation:
+Chef was not able to solve at least 10 problems in any of the four weeks. Hence, the number of weeks in which Chef met his target is 0.
+* */
+
+public class PracticeMakesUsPerfect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
+        int count=0;
+        for(int x=0; x<arr.length; x++){
+            int numOfSolved=sc.nextInt();
+            if(numOfSolved>=10){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/CodeChef/Basic/Insurance.java
@@ -0,0 +1,65 @@
+package CodeChef.Basic;
+
+import java.util.Scanner;
+
+/*
+Problem
+Chef bought car insurance. The policy of the insurance is:
+
+The maximum rebatable amount for any damage is Rs X lakhs.
+If the amount required for repairing the damage is \leq X≤X lakhs, that amount is rebated in full.
+Chef's car meets an accident and required Rs Y lakhs for repairing.
+
+Determine the amount that will be rebated by the insurance company.
+
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases.
+The first and only line of each test case contains two space-separated integers X and Y.
+Output Format
+For each test case, output the amount (in lakhs) that will be rebated by the insurance company.
+
+Constraints
+1≤T≤1000
+1≤X,Y≤30
+Sample 1:
+Input
+4
+5 3
+5 8
+4 4
+15 12
+Output
+3
+5
+4
+12
+Explanation:
+Test case 1: The damages require only Rs 3 lakh which is below the upper cap, so the entire Rs 3 lakh will be rebated.
+
+Test case 2: The damages require Rs 8 lakh which is above the upper cap, so only Rs 5 lakh will be rebated.
+
+Test case 3: The damages require only Rs 4 lakh which is equal to the upper cap, so the whole Rs 4 lakh will be rebated.
+
+Test case 4: The damages require Rs 15 lakh which is above the upper cap, so only Rs 12 lakh will be rebated.
+
+
+* */
+
+public class Insurance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int x=0; x<T; x++){
+            int maxRebate=sc.nextInt();
+            int requiredAmt=sc.nextInt();
+
+            if(requiredAmt<=maxRebate){
+                System.out.println(requiredAmt);
+            } else{
+                System.out.println(maxRebate);
+            }
+
+        }
+    }
+}
